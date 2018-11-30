ac6b23c7e0e5db5c35340de3387cc2cc1454341d
tobelesa <45403307+tobelesa@users.noreply.github.com>
2018-11-30 10:06:25 +0100

    Add files via upload

--- modele.ml
+++ modele.ml
@@ -1,24 +1,41 @@
 type t =
-    { Compatible_maneuvers : int list array; (*les Di*)
-      Planes_left : int list; };;
+    { compatible_maneuvers: int list array; (*les Di*)
+      planes_left: int list };;
 
-exception EmptyCompatibleManeuvers(i)
+exception EmptyCompatibleManeuvers of int;;
+exception EmptyPlanesLeftList;;
 
-let head t i =
-  match t.Compatible_maneuvers.(i) with
+let head_compatible_maneuvers t i =
+  match t.compatible_maneuvers.(i) with
   |a::b -> a
-  |[] -> raise EmptyCompatibleManeuvers(i);;
+  |[] -> raise (EmptyCompatibleManeuvers i)
+;;
 
-let tail t i =
-  match t.Compatible_maneuvers.(i) with
+
+let tail_compatible_maneuvers t i =
+  match t.compatible_maneuvers.(i) with
   |a::b -> b
-  |[] -> raise EmptyCompatibleManeuvers(i);;
+  |[] -> raise (EmptyCompatibleManeuvers i)
+;;
+
+let pop_planes_left t =
+  match t.planes_left with
+  |a::b -> b
+  |[] -> raise EmptyPlanesLeftList;;
+
+let empty_planes_left t =
+  match t.planes_left with
+  |[] -> true
+  |a::b -> false;;
+
 
 let create_reste t =
   (*renvoie le nouveau sous-problème S'*)
+  ()
 ;;
 
 let create_prime t =
   (*renvoie le nouveau sous-problème Sreste*)
+  ()
   ;;
     
