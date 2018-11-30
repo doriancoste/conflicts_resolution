2177cf50ed4b48f316c027803e64a597d1494ca0
tobelesa <45403307+tobelesa@users.noreply.github.com>
2018-11-30 11:32:47 +0100

    Add files via upload

--- modele.ml
+++ modele.ml
@@ -5,6 +5,9 @@ type t =
 exception EmptyCompatibleManeuvers of int;;
 exception EmptyPlanesLeftList;;
 
+let get_compatible_maneuvers t i =
+  t.compatible_maneuvers.(i);;
+
 let head_compatible_maneuvers t i =
   match t.compatible_maneuvers.(i) with
   |a::b -> a
@@ -21,13 +24,14 @@ let tail_compatible_maneuvers t i =
 let pop_planes_left t =
   match t.planes_left with
   |a::b -> b
-  |[] -> raise EmptyPlanesLeftList;;
+  |[] -> raise EmptyPlanesLeftList
+;;
 
-let empty_planes_left t =
+let is_empty_planes_left t =
   match t.planes_left with
   |[] -> true
-  |a::b -> false;;
-
+  |a::b -> false
+;;
 
 let create_reste t =
   (*renvoie le nouveau sous-problème S'*)
@@ -37,5 +41,54 @@ let create_reste t =
 let create_prime t =
   (*renvoie le nouveau sous-problème Sreste*)
   ()
-  ;;
+;;
+
+let filtre = fun i s m->
+  (* 
+     m : bool array array array array
+     s : type t {compatible_maneuvers: int list array; planes_left: int list}  
+     i : ième avion (int)
+   *)
+  let c_maneuvers = s.compatible_maneuvers in
+  let p_left = s.planes_left in
+  let n = Array.length c_maneuvers in (* n planes in total*)
+  let n_left = List.length p_left in
+  let n_comp = n - n_left in (*nombre of planes compatible*)
+  
+  (*jeme plane, maneuvres of jeme plane, one maneuvre of ieme plane, compatible or not, return compatible or not with j*)
+  (*If there is no conflit, return true*)
+  let rec j_match_rec = fun j list_j xi result->
+    match list_j with
+      [] -> result
+    | xj::tail ->
+      let no_conflit = m.(i).(j).(xi).(xj) in
+      let result = (no_conflit && result) in
+      if (result = true) then
+        j_match_rec j tail xi result
+      else
+        result
+  in
+  
+  (*one maneuvre of ieme plane, return compatible or not with all j in c_maneuvers *)
+  (*If there is no conflit, return true*)
+  let rec match_rec = fun xi->
+    let result  = ref true in
+    for j=0 to n_comp -1 do
+      result := !result && (j_match_rec j c_maneuvers.(j) xi true)
+    done;
+    !result  
+  in
+  
+  (*maneuvres of ieme plane, maneuvres compatible of ieme plane*)
+  let rec filtre_rec = fun list_i result->
+    match list_i with
+      [] -> result
+    | xi::tail ->
+        if (match_rec xi) then
+          filtre_rec tail (xi::result)
+        else
+          filtre_rec tail result
+  in
+  filtre_rec c_maneuvers.(i) []
+    
     
--- modele.mli
+++ modele.mli
@@ -2,22 +2,38 @@ type t = { compatible_maneuvers : int list array; planes_left : int list; };
 (*Type modèle, associé à une priorité et stocké dans la pqueue*)
 
 
-exception EmptyCompatibleManeuvers of int;
-exception EmptyPlanesLeftList;
+exception EmptyCompatibleManeuvers of int
+exception EmptyPlanesLeftList
 
-val get_compatible_maneuvers : t -> int -> int list;
+val init: int array -> int -> t
+(**Crée le premier problème, avec (int array) les couts (qui donne
+   le nombre de manoeuvres et (int) le nombre d'avions.*)
 
+val get_compatible_maneuvers : t -> int -> int list
+(**Choppe le tabelau des maneuvres compatibles restantes
+   pour l'avion i : Di *)
+ 
+val head_compatible_maneuvers : t -> int -> int
+(**Choppe la première manoeuvre compatible de la liste. Renvoie 
+   l'exception EmptyCompatibleManeuvers si liste vide*)
 
-val head_compatible_maneuvers : t -> int -> int;
+val tail_compatible_maneuvers : t -> int -> int list
+ 
+(**Choppe les  manoeuvres compatible de la liste, avec la 
+   première en moins. Renvoie l'exception EmptyCompatibleManeuvers
+   si liste vide*)
+ 
+val del_i_planes_left : t -> int -> int list
+(**Supprime l'indice i dans la liste planes_left*)
 
-val tail_compatible_maneuvers : t -> int -> int list;
+val is_empty_planes_left : t -> bool
+(**renvoie true si la liste des avions restants est vide*)
 
-val pop_planes_left : t -> int list;
+val create_reste : t -> t
+(**renvoie le nouveau sous-problème S'*)
 
-val is_empty_planes_left : t -> bool;
+val create_prime : t -> t
+(**renvoie le nouveau sous-problème Sreste*)
 
-val create_reste : t -> t;
-  (*renvoie le nouveau sous-problème S'*)
-
-val create_prime : t -> t;
-  (*renvoie le nouveau sous-problème Sreste*)
+val get_priority : t -> int array -> int
+(**Calcule la priorité associée à l'objet t*)
