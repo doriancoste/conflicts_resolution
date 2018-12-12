41c103ac3f830771a81005998887e6cd5272f7c8
doriancoste <dorian.coste.cst@gmail.com>
2018-12-12 09:30:34 +0100

    Update solve.ml
    application de la convention notation

--- solve.ml
+++ solve.ml
@@ -3,48 +3,48 @@ let () =
   (* on charge d'abord le dichier du cluster puis on construit le sommet de l'arbre que l'on insert dans la file a priorite *)
   (* let fichier = "conflicts.txt" in *)
   let fichier = String.concat "" ["./conflicts/";Load_data.select_data_file ()] in
-  let cost,no_conflict,nb_avion = Load_data.load fichier in
-  let s = Modele.init cost nb_avion in
-  let bound = Load_data.select_bound no_conflict nb_avion in
+  let cost,no_conflict,nb_planes = Load_data.load fichier in
+  let s = Modele.init cost nb_planes in
+  let bound = Load_data.select_bound no_conflict nb_planes in
   let filter = Load_data.select_method () in
-  let prio = bound s cost in
-  let q = Pqueue.insert prio s Pqueue.empty in   (* Creer la file initiale *)
+  let priority = bound s cost in
+  let q = Pqueue.insert priority s Pqueue.empty in   (* Creer la file initiale *)
   let count = ref 0 in
   let start = Sys.time () in
   (* on utilise une fonction récursive qui va depiler les elements de q jusqu'a trouver une solution, en ajoutant les voisin a q *)
   let rec solve_rec = fun q ->
     count:=!count+1;
-    let prio, s, q = Pqueue.extract q in
+    let priority, s, q = Pqueue.extract q in
     match s.Modele.planes_left with
     (* plus d'avions a instancier = sol trouvee *)
-      [] -> (s.compatible_maneuvers, prio)
+      [] -> (s.compatible_maneuvers, priority)
     | _ ->
       (* on recupere l'id de l'avion a instancier *)
       let plane_id = List.hd s.Modele.planes_left in
       let d_tot = s.compatible_maneuvers in
 
       (* on recupere le numero de manoeuvre que l'on va affecter a plane_id *)
-      let maneuver_of_id = List.hd s.compatible_maneuvers.(plane_id) in
+      let maneuver_id = List.hd s.compatible_maneuvers.(plane_id) in
 
-      (* liste des maneuvres pour plane_id  si plane_id n'effectue PAS maneuver_of_id *)
+      (* liste des maneuvres pour plane_id  si plane_id n'effectue PAS maneuver_id *)
       let dr_id = List.tl s.compatible_maneuvers.(plane_id) in
-      (* liste des maneuvres restantes si plane_id effectue maneuver_of_id (soit le singleton maneuver_of_id) *)
-      let d_id = [maneuver_of_id] in
+      (* liste des maneuvres restantes si plane_id effectue maneuver_id (soit le singleton maneuver_id) *)
+      let d_id = [maneuver_id] in
 
-      (* tableau des domaines de maneuvres compatibles au cas plane_id n'effectue PAS maneuver_of_id *)
+      (* tableau des domaines de maneuvres compatibles au cas plane_id n'effectue PAS maneuver_id *)
       let dr_tot = Array.copy d_tot in
       Array.set dr_tot plane_id dr_id;
 
-      (* tableau des domaines de maneuvres compatibles au cas plane_id effectue maneuver_of_id *)
+      (* tableau des domaines de maneuvres compatibles au cas plane_id effectue maneuver_id *)
       let dnew_tot = Array.copy d_tot in
       Array.set d_tot plane_id d_id;
 
-      (* noeud de l'arbre contenant les branches ou plane_id n'effectue PAS maneuver_of_id *)
+      (* noeud de l'arbre contenant les branches ou plane_id n'effectue PAS maneuver_id *)
       let sr = Modele.make dr_tot s.planes_left in
 
-      (* noeud de l'arbre contenant les branches ou plane_id effectue maneuver_of_id *)
+      (* noeud de l'arbre contenant les branches ou plane_id effectue maneuver_id *)
       let s_no_filtered = Modele.make dnew_tot (List.tl s.planes_left) in
-      let s_new = filter plane_id maneuver_of_id s_no_filtered no_conflict in
+      let s_new = filter plane_id maneuver_id s_no_filtered no_conflict in
 
       (* on ajoute sr et s_new a la file q *)
       let new_q = Pqueue.insert (Priority.get_priority_1 sr cost) sr q in
@@ -55,7 +55,7 @@ let () =
   let maneuvers_sol,cost_tot = solve_rec q in
   let passed_time = Sys.time () -. start in
   Printf.printf "cout de la solution: %d\nnoeud exploré: %d\n\n" cost_tot !count;
-  (for i=0 to nb_avion-1 do
+  (for i=0 to nb_planes-1 do
     Printf.printf "L'avion %d effectue la manoeuvre: %d\n" (i+1) (List.hd maneuvers_sol.(i));
   done);
   Printf.printf "\nSolution trouvée en %f seconde(s).\n\n\n" passed_time;;
