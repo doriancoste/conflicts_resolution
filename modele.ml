e8dd96d49dff6c509160fa76c5bd0418b2b71188
ZHEN <45403368+jerryznli@users.noreply.github.com>
2018-12-12 09:55:17 +0100

    Add files via upload

--- modele.ml
+++ modele.ml
@@ -13,14 +13,14 @@ let create_list n f =
   creat_rec (n-1) [];;
 
 
-let init cost n =
+let init cost nb_planes =
   let nombre_de_manoeuvres = Array.length cost in
   let liste_di = create_list nombre_de_manoeuvres (fun i -> i) in
 
   let liste_di_sorted = List.sort (fun i j -> cost.(i)-cost.(j)) liste_di in (*Tri des manoeuvres, la moins chère d'abord*)
 
-  let a = Array.init n (fun _ -> liste_di_sorted) in
-  let b = create_list n (fun i -> i) in
+  let a = Array.init nb_planes (fun _ -> liste_di_sorted) in
+  let b = create_list nb_planes (fun i -> i) in
   {compatible_maneuvers= a;
    planes_left = b };;
 
@@ -34,35 +34,35 @@ retour : type t; represente un noeud
 
 
 
-let get_compatible_maneuvers t i =
-  t.compatible_maneuvers.(i);;
+let get_compatible_maneuvers s i =
+  s.compatible_maneuvers.(i);;
 
-let head_compatible_maneuvers t i =
-  match t.compatible_maneuvers.(i) with
+let head_compatible_maneuvers s i =
+  match s.compatible_maneuvers.(i) with
   |a::b -> a
   |[] -> raise (EmptyCompatibleManeuvers i)
 ;;
 
 
-let tail_compatible_maneuvers t i =
-  match t.compatible_maneuvers.(i) with
+let tail_compatible_maneuvers s i =
+  match s.compatible_maneuvers.(i) with
   |a::b -> b
   |[] -> raise (EmptyCompatibleManeuvers i)
 ;;
 
-let pop_planes_left t =
-  match t.planes_left with
+let pop_planes_left s =
+  match s.planes_left with
    a::b -> b
   |[] -> raise EmptyPlanesLeftList
 ;;
 
-let is_empty_planes_left t =
-  match t.planes_left with
+let is_empty_planes_left s =
+  match s.planes_left with
    [] -> true
   |a::b -> false
 ;;
 
-let filter = fun i maneuveri s no_conflict ->
+let filter = fun i maneuver_i s no_conflict ->
   (* on creer une fonction qui a partir de i, de j, de maneuveri, de la matrice des conflits et de la liste
 des manoeuvres possibes pour dj, renvoie la liste des maneuvre possible pour dj sachant que xi=maneuveri *)
 
@@ -71,7 +71,7 @@ des manoeuvres possibes pour dj, renvoie la liste des maneuvre possible pour dj 
     match dj_list with
       [] -> List.rev new_dj_list
     | hd::tl ->
-      if no_conflict.(i).(j).(maneuveri).(hd) then
+      if no_conflict.(i).(j).(maneuver_i).(hd) then
         let new_dj_list = hd::new_dj_list in
         dj_to_newdj j tl new_dj_list
       else
@@ -86,12 +86,12 @@ des manoeuvres possibes pour dj, renvoie la liste des maneuvre possible pour dj 
       Array.set d_array hd (dj_to_newdj hd d_array.(hd) []);
       let new_planes_left = hd::new_planes_left in
       browse_D tl d_array new_planes_left in
-
+  
   (* on renvoie le noeud s apres y avoir applique le filtre *)
   let darray,p_left = browse_D s.planes_left s.compatible_maneuvers [] in
   make darray p_left;;
 
-  let ac3 = fun i j maneuver_i s no_conflict ->
+let ac3 = fun i j maneuver_i s no_conflict ->
     let c_maneuvers = s.compatible_maneuvers in
     let rec xi_match_rec = fun list_j result->
       match list_j with
@@ -105,13 +105,13 @@ des manoeuvres possibes pour dj, renvoie la liste des maneuvre possible pour dj 
     Array.set c_maneuvers j (xi_match_rec c_maneuvers.(j) []);
     make c_maneuvers s.planes_left;;
 
-let filter_ac3 = fun i maneuveri s no_conflict ->
+let filter_ac3 = fun i maneuver_i s no_conflict ->
   let n = Array.length s.compatible_maneuvers in (* n planes in total*)
   let result = ref s in
   for j=0 to n-1 do
     begin
       if j !=i then
-        result := ac3 i j maneuveri !result no_conflict;
+        result := ac3 i j maneuver_i !result no_conflict;
     end
   done;
   s;;
