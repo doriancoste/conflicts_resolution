5e84a80b6135b421d20e2b990b61e76eb0dd0894
doriancoste <dorian.coste.cst@gmail.com>
2018-12-21 10:48:06 +0100

    correction du script et d'une erreur sur le fichier cluster_15ac_3err_8.txt

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ if [[ $# -ne 3 ]]
 then
 	echo "Erreur: mauvais nombre de parametre"
 	echo "1ème parametre doit est 'if' ou 'pas_if'"
-	echo "2ème parametre doit est 'bnb' ou 'ac3'"
+	echo "2ème parametre doit est 'bandb' ou 'ac3'"
 	echo "3ème parametre doit est 'naive' ou 'mij'"
 	exit 1
 fi
@@ -27,12 +27,14 @@ do
 	do
 		for r in 0 1 2 3 4 5 6 7 8 9
 		do
-			#echo "n=$n e=$e r=$r" 
+			#echo "n=$n e=$e r=$r"
 			if [ $1 = "pas_fi" ]
 			then
-				./conflicts_resolution -n $n -e $e -r $r -fi -m $2 -b $3 >> result
+				echo "instance $n $e $r">>result.txt
+				./conflicts_resolution -n $n -e $e -r $r -fi -m $2 -b $3 >> result.txt
 			else
-				./conflicts_resolution -n $n -e $e -r $r -m $2 -b $3 >> result
+				echo "instance $n $e $r">>result.txt
+				./conflicts_resolution -n $n -e $e -r $r -m $2 -b $3 >> result.txt
 			fi
 		done
 
